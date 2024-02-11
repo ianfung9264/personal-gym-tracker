6600bf3583ecd49687308168e94fcb44d5ec0642
Tsz Yin Ian Fung <ian@Tszs-MacBook-Pro.local>
2024-02-12 01:07:40 +0800

    fix env variable

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,20 +1,16 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCxlpyl8l7aYGtoxwbN_UWGE-yH2uSu_6g",
-  authDomain: "fir-auth-84d5b.firebaseapp.com",
-  projectId: "fir-auth-84d5b",
-  storageBucket: "fir-auth-84d5b.appspot.com",
-  messagingSenderId: "604288596005",
-  appId: "1:604288596005:web:f87c966f6fbc62e18d67cc"
-};
-
-// Initialize Firebase
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+  };
+  
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 
